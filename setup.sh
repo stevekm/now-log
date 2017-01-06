76da8f3d980fc675630009cb7f95d4043688db66
Stephen Kelly <stevekm@users.noreply.github.com>
2017-01-06 12:59:49 -0500

    fix setup script path

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@
 ## USAGE: setup.sh
 ## DESCRIPTION: Add now-logger to bashrc
 
-now_logger_filepath="${PWD}/now-logger.sh)"
+now_logger_filepath="${PWD}/now-logger.sh"
 
 if [ -f "$now_logger_filepath" ]; then
+    echo "Adding $now_logger_filepath to ~/.bashrc"
     echo "source $now_logger_filepath" >> ~/.bashrc
 else
     echo "ERROR: $now_logger_filepath is not a file. Does it exist?"
