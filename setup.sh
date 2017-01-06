2076908d1abc8b784159d617c6c4d35a4529503f
Stephen Kelly <stevekm@users.noreply.github.com>
2017-01-06 13:00:55 -0500

    update setup messages

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ now_logger_filepath="${PWD}/now-logger.sh"
 if [ -f "$now_logger_filepath" ]; then
     echo "Adding $now_logger_filepath to ~/.bashrc"
     echo "source $now_logger_filepath" >> ~/.bashrc
+    echo "You should start a new terminal session now."
 else
     echo "ERROR: $now_logger_filepath is not a file. Does it exist?"
 fi
